642421d537961096e31d6577613cfe9aa91ed2d7
Thyago Monteiro <tmacademico@gmail.com>
2024-02-01 22:08:13 -0300

    Implementação de validator personalizado

---
+++ src/main/java/com/thymont/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,38 @@
+package com.thymont.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.thymont.domain.enums.TipoCliente;
+import com.thymont.dto.ClienteNewDTO;
+import com.thymont.resources.exception.FieldMessage;
+import com.thymont.services.validation.utils.BR;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	@Override
+	public void initialize(ClienteInsert ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+		List<FieldMessage> list = new ArrayList<>();
+
+		if (objDto.getTipo().equals(TipoCliente.PESSOA_FISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CPF Inválido"));
+		} 
+		
+		if ((objDto.getTipo().equals(TipoCliente.PESSOA_JURIDICA.getCod())) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CNPJ Inválido"));
+		} 
+
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
