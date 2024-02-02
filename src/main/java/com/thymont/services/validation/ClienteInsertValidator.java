eeb1d88fea6fc9db0f04c43f41616c868a0bf550
Thyago Monteiro <tmacademico@gmail.com>
2024-02-01 22:14:34 -0300

    Validação email não repetido

--- src/main/java/com/thymont/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/thymont/services/validation/ClienteInsertValidator.java
@@ -3,8 +3,12 @@ package com.thymont.services.validation;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.thymont.domain.Cliente;
 import com.thymont.domain.enums.TipoCliente;
 import com.thymont.dto.ClienteNewDTO;
+import com.thymont.repositories.ClienteRepository;
 import com.thymont.resources.exception.FieldMessage;
 import com.thymont.services.validation.utils.BR;
 
@@ -12,6 +16,10 @@ import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -27,6 +35,12 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		if ((objDto.getTipo().equals(TipoCliente.PESSOA_JURIDICA.getCod())) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ Inválido"));
 		} 
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if( aux!= null) {
+			list.add(new FieldMessage("email", "Email já existente"));
+		}
+			
 
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
