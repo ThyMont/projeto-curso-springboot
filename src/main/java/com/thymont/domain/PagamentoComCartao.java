3731be07451ab446a6470eb20e444dc02b6453b8
Thyago Monteiro <tmacademico@gmail.com>
2024-02-04 18:17:43 -0300

    inserção de novos pedidos

--- src/main/java/com/thymont/domain/PagamentoComCartao.java
+++ src/main/java/com/thymont/domain/PagamentoComCartao.java
@@ -1,10 +1,12 @@
 package com.thymont.domain;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.thymont.domain.enums.EstadoPagamento;
 
 import jakarta.persistence.Entity;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento {
 	private static final long serialVersionUID = 1L;
 
